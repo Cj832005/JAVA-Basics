682b3568eb56096b47c778af680d584befd005cb
Cj832005 <sajad.tir93@gmail.com>
2025-10-15 21:37:36 +0330

    solved3

--- taklif/src/Soal3.java
+++ taklif/src/Soal3.java
@@ -1,7 +1,53 @@
+import java.util.Scanner;
+
 public class Soal3 {
     public static void main(String[] args) {
+    int n1 , n2 ,Result ;
+    String choice;
+    Scanner scanner = new Scanner(System.in);
+
+    while(true){
+        System.out.print("Enter num1 : ");
+        n1 = scanner.nextInt();
+        System.out.print("Enter num2 : ");
+        n2 = scanner.nextInt();
+        System.out.print("Choice: 1.Add 2.Subtract 3.Multiply 4.Divide : ");
+        choice = scanner.next();
+        if(choice.equals("1")  || choice.equals("+") ){
+            Result =n1+n2 ;
+            System.out.println("Result =" + Result);
+        }
+        else if(choice.equals("2") || choice.equals("-") ){
+            Result = n2>n1 ? n2-n1 : n1-n2;
+            System.out.println("Result =" + Result);
+        }
+        else if(choice.equals("3") || choice.equals("*") ){
+            Result =n1*n2;
+            System.out.println("Result =" + Result);
+        }
+        else if(choice.equals("4") || choice.equals("/") ){
+            try {
+                Result = n2>n1 ? n2/n1 : n1/n2;
+                System.out.println("Result =" + Result);
+            }
+            catch (ArithmeticException e) {
+
+                System.out.println("Error: Division by zero!!!");
+                break;
+            }
+        }
+       System.out.println();
+
+
+
+
     }
-    }
 
 
 
+
+
+
+
+}
+}
